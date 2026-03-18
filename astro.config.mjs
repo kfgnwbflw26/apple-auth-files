4fa0c0b8ecf4383fae5c0f8742f41384e121b868
kfgnwbflw26 <khris.francia@genomics.com>
2026-03-19 00:10:54 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 import { defineConfig } from 'astro/config';
 export default defineConfig({
-  outDir: './dist',
-  publicDir: './public'
+  output: 'static',
+  build: {
+    format: 'file'
+  }
 });
